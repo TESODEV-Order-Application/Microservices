6208fb7675dfc2586d66e541dd1822d7c3ed8e34
Raymon01 <ramonberke01@gmail.com>
2024-08-21 09:48:37 +0300

    chore: Remove unused email functionality and dependencies

--- order_service/main.py
+++ order_service/main.py
@@ -1,27 +1,10 @@
 from fastapi import FastAPI
 from app.routes import router
 import uvicorn
-from fastapi_utils.tasks import repeat_every
 
 app = FastAPI()
 
 app.include_router(router)
-
-
-
-
-
-@app.on_event("startup")
-@repeat_every(wait_first=False,seconds=10)
-async def startup_event():
-    print("Email Loading Sequence has Started")
-    #result = "await loadAllNews()"
-    #print(result)
-
-
-
-
-
 
 if __name__ == "__main__": 
     uvicorn.run(app, host="0.0.0.0", port=8002)
